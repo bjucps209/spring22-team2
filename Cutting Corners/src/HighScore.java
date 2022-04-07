243655f693a8f0c4b785dd4c707aea95d2c541da
ethancollins0205@gmail.com <ethancollins0205@gmail.com>
2022-04-07 16:19:53 -0700

    Implemented High Score Methods/Tests

--- Cutting Corners/src/HighScore.java
+++ Cutting Corners/src/HighScore.java
@@ -1,9 +1,9 @@
-public class HighScore {
+public class HighScore implements Comparable {
     private int score;
     private String name;
     public HighScore()
     {
-        
+
     }
     public HighScore(int s, String n)
     {
@@ -21,6 +21,29 @@ public class HighScore {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    @Override
+    public int compareTo(Object o) {
+        if(o instanceof HighScore)
+        {
+            HighScore temp = (HighScore) o;
+            if(this.getScore()>temp.getScore())
+            {
+                return 1;
+            }
+            else if(this.getScore()<temp.getScore())
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            return 0;
+        }
     }
     
 }
--- Cutting Corners/src/HighScoreManager.java
+++ Cutting Corners/src/HighScoreManager.java
@@ -6,22 +6,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+//I am aware that we were not supposed to implement the methods as part of the design step, but I had the time to do so, and I need to make the most of the time
+//I have to make sure everything gets done
 public class HighScoreManager {
     private HighScore[] scores = new HighScore[5];
-    public HighScoreManager() throws IOException
+    public HighScoreManager()
     {
-        try (DataInputStream reader = new DataInputStream(new FileInputStream("scores.dat")))
+        try
         {
-            for(int i=0;i<5;i++)
-            {
-                HighScore score = new HighScore();
-                score.setScore(reader.readInt());
-                score.setName(reader.readUTF());
-                scores[i]=score;
-            }
+        this.load();
         }
-        catch(EOFException e)
+        catch (IOException e)
         {
             for(int i=0;i<5;i++)
             {
@@ -71,5 +66,20 @@ public class HighScoreManager {
                 writer.writeUTF(scores[i].getName());
             }
         }
+    }
+    public void resetScores()
+    {
+        for(int i=0;i<5;i++)
+        {
+            scores[i]=new HighScore((i+1)*1000,"Default");
+        }
+    }
+    public HighScore get(int i)
+    {
+        return scores[i];
+    }
+    public void setScores(HighScore[] scores)
+    {
+        this.scores=scores;
     }
 }
