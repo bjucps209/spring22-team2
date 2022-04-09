2c2af5c053fb9a3425d8ee85429f252650a3429d
Paul Alger <palge667@students.bju.edu>
2022-04-08 21:50:02 -0400

    Added save/load

---
+++ Cutting Corners/src/SerializeTest.java
@@ -0,0 +1,17 @@
+// import static org.junit.Assert.assertEquals;
+// import model.*;
+
+// public class SerializeTest {
+
+//     public World makeTestWorld() {
+
+//         World testWorld = new World();
+
+//         return testWorld;
+//     }
+
+//     @Test
+//     void testSave_savesToFile() {
+
+//     }
+// }
