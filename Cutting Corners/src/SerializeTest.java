092e6e19b827801bcd55d950b635caac043157e5
Paul Alger <palge667@students.bju.edu>
2022-04-08 22:39:59 -0400

    "Added load/save"

--- Cutting Corners/src/SerializeTest.java
+++ Cutting Corners/src/SerializeTest.java
@@ -1,17 +1,31 @@
-// import static org.junit.Assert.assertEquals;
-// import model.*;
+import static org.junit.Assert.assertEquals;
 
-// public class SerializeTest {
+import org.junit.Test;
 
-//     public World makeTestWorld() {
+import model.*;
 
-//         World testWorld = new World();
+public class SerializeTest {
 
-//         return testWorld;
-//     }
 
-//     @Test
-//     void testSave_savesToFile() {
+    @Test
+    void testSave_savesToFile() {
+        // World.reset();
+        // Player player = new Player(1, 2);
+        // Enemy enemy = new Enemy(3, 5, 5, 5, null);
+        // World.instance().getCampaign().add(new Level(2, 1));
+        // World.instance().getCurrentLevel().placeEntity(1, 2, player);
+        // World.instance().getCurrentLevel().placeEntity(2, 4, enemy);
+        World.instance().save("SaveGame.dat");
 
-//     }
-// }
+    }
+
+    @Test
+    void testLoad_loadsFromFile() {
+        // World.reset();
+        // Level currentLevel = World.instance().getCurrentLevel();
+        // assertEquals(currentLevel.getEntities().size(), 0);
+        World.instance().load("SaveGame.dat");
+        // assertEquals(currentLevel.getEntities().size(), 2);
+
+    }
+}
