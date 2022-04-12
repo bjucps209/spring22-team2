89b6be57e0b09f8cce2f2c9e0630773e9a1eb23d
ethancollins0205@gmail.com <ethancollins0205@gmail.com>
2022-04-12 14:48:27 -0700

    Fixed/Updated HighScoreTest

--- Cutting Corners/src/HighScoreTest.java
+++ Cutting Corners/src/HighScoreTest.java
@@ -1,8 +1,12 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
 
 import org.junit.Test;
 public class HighScoreTest {
     public HighScoreManager scores = new HighScoreManager();
+    HighScoreManager tempScores = new HighScoreManager();
     @Test
     public void testReset()
     {
@@ -16,14 +20,50 @@ public class HighScoreTest {
     @Test
     public void testSort()
     {
-        HighScore[] tempScores = new HighScore[5];
-        tempScores[0] = new HighScore(10,"Ten");
-        tempScores[1] = new HighScore(4,"Four");
-        tempScores[2] = new HighScore(9,"Nine");
-        tempScores[3] = new HighScore(12,"Twelve");
-        tempScores[4] = new HighScore(1,"One");
-        scores.setScores(tempScores);
+        HighScore[] temp = new HighScore[5];
+        temp[0] = new HighScore(10,"Ten");
+        temp[1] = new HighScore(4,"Four");
+        temp[2] = new HighScore(9,"Nine");
+        temp[3] = new HighScore(12,"Twelve");
+        temp[4] = new HighScore(1,"One");
+        scores.setScores(temp);
+        tempScores.setScores(temp);
         scores.sort();
         assertEquals(1, scores.get(0).getScore());
+    }
+    @Test
+    public void testSaveLoad()
+    {
+        try
+        {
+            scores.save();
+            scores.resetScores();
+            scores.load();
+        }
+        catch (IOException e)
+        {
+            fail();
+        }
+        for(int i=0;i<5;i++)
+        {
+            assertEquals(tempScores.get(i).getName(), scores.get(i).getName());
+            assertEquals(tempScores.get(i).getScore(), scores.get(i).getScore());
+        }
+    }
+    @Test
+    public void testAdd()
+    {
+        HighScore[] temp = new HighScore[5];
+        temp[0] = new HighScore(10,"Ten");
+        temp[1] = new HighScore(4,"Four");
+        temp[2] = new HighScore(9,"Nine");
+        temp[3] = new HighScore(12,"Twelve");
+        temp[4] = new HighScore(1,"One");
+        scores.setScores(temp);
+        tempScores.setScores(temp);
+        scores.sort();
+        scores.addScore(new HighScore(7,"Seven"));
+        assertEquals(7, scores.get(1).getScore()); 
+        assertEquals(4, scores.get(0).getScore());  
     }
 }
