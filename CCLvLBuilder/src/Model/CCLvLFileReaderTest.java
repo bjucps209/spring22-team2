6b4222b5d8ef855608331ee03e65ccd3d20c0b85
smeye584 <smeye584@students.bju.edu>
2022-04-26 14:22:24 -0400

    LvLB

--- CCLvLBuilder/src/Model/CCLvLFileReaderTest.java
+++ CCLvLBuilder/src/Model/CCLvLFileReaderTest.java
@@ -1,26 +1,40 @@
 package Model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class CCLvLFileReaderTest {
     DataManager DM = DataManager.DaMan();
 
-/*     @Test
-    public void test_ReaderTest_TestLoad(){
+    @Test
+    public void test_ReaderTest_TestSaveandLoad(){
         DM.resetDaMan();
 
-        DM.load("Pathway");
-        
-        assertEquals( 9, DM.getScreens().size()); ///
+        DM.attemptCreateScreen(Direction.North);
+        DM.createObject("Player", "imgthing.png", ObjType.Player, new Vector(2, 2), new Vector(1, 1));
+
+        try {
+            CCLvLFileReader.save(DM.getTheLevel(), "TestyTest", false);        
+        } catch (Exception e) {
+            fail();
+        }
+        try {
+            DM.load("TestyTest");
+        } catch (Exception e) {
+            fail();
+        }
+
+        assertEquals( 2, DM.getScreens().size()); ///
         assert DM.getCurrentScreen() != null; ///
 
-        String playerLoc = DM.getPlayerLoc(); //Not implemented yet (Could switch to Screen from StrID)
+        String playerLoc = DM.getPlayerScrID(); //Not implemented yet (Could switch to Screen from StrID)
+        assertEquals("0,1,0", playerLoc);
         int incr = 0;
         for (Screen sc : DM.getScreens()) {
 
-            if(sc.getStrID().equals("blah")) { //change to playerLoc
+            if(sc.getStrID().equals(playerLoc)) { //change to playerLoc
                 boolean testthing = false;
                 for(LvLObject obj : sc.getObjects()) {
                     if (obj.getName().equals("Player")) {
@@ -32,6 +46,6 @@ public class CCLvLFileReaderTest {
 
             incr += sc.getObjects().size();
         }
-        assertEquals(14, incr); ///
-    } */
+        assertEquals( 2, incr); ///
+    }
 }
